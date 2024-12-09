a74e0e1b773886118e2e139ba9cffb242272604e
台灣智能產品開發-林聖惇(Mars Lin) <mars.lin@meiloon.com>
2024-12-09 10:05:55 +0800

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ML_package",
+    platforms: [
+        .iOS(.v15), // 設置最低支援版本
+    ],
+    products: [
+        .library(
+            name: "ML_package",
+            targets: ["ML_package"]
+        ),
+    ],
+    dependencies: [
+        // 添加依賴
+        .package(url: "https://github.com/crelies/AdvancedList.git", from: "1.4.0"),
+        .package(url: "https://github.com/spacenation/swiftui-sliders.git", from: "2.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "ML_package",
+            dependencies: [
+                "AdvancedList",
+                "swiftui-sliders"
+            ],
+            path: "Sources"
+        ),
+        .testTarget(
+            name: "ML_packageTests",
+            dependencies: ["ML_package"],
+            path: "Tests"
+        ),
+    ]
+)
